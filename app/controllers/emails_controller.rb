4776b11831bd53f0545e89d17333585f2db75770
shahoxo <mottipok@hotmail.com>
2012-12-10 16:57:31 +0900

    asd

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,8 +1,8 @@
 class EmailsController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:new]
   def new
-    #render json: {'res' => 'not duplicated'}
-    render text: "test!"
+    render json: {'res' => 'not duplicated'}
+    #render text: "test!"
   end
 
   def confirm
